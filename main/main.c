695948b9b5371f75016ae6283ae6657d6009757f
TortugaDiabetica <felipecastillo1604@gmail.com>
2026-03-26 22:27:49 -0300

    major-change/Actualizacion de ESP IDF de 5.5.1 a 6.0

--- main/main.c
+++ main/main.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <driver/gpio.h>
-#include "app_sensor.h"
-#include "app_wifi_init.h"
-#include "app_mqtt_client.h"
 
 void app_main(void)
 {
-    printf("=== Sistema de Monitoreo IoT ===\n");
-    wifi_init();
-    mqtt_init();
-    // Crear tarea para el sensor DHT11
-    xTaskCreate(readDHT11, "Lectura sensor DHT11", 4096, NULL, 1, NULL);
+
 }
